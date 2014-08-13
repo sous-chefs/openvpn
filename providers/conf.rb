801950d73abcacc0fe254cc29ebb379076d921ae
James Le Cuirot <james.le-cuirot@yakara.com>
2014-08-13 16:25:16 +0100

    Always use openvpn cookbook for server template
    The nature of the template means that you wouldn't even need to use a
    custom version. Resolves #21.

--- providers/conf.rb
+++ providers/conf.rb
@@ -20,6 +20,7 @@ use_inline_resources if defined?(use_inline_resources)
 
 action :create do
   template "/etc/openvpn/#{new_resource.name}.conf" do
+    cookbook 'openvpn'
     source 'server.conf.erb'
     owner 'root'
     group 'root'
