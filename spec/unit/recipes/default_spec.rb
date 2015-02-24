96dd83bfb300ecaa6290b5ec8c8fcd37ab3c9e46
Chris Fordham <chris@xhost.com.au>
2015-02-24 11:48:00 +1100

    Use ChefSpec::SoloRunner instead of ChefSpec::Runner' due to deprecation.

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe 'openvpn::default' do
-  let(:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
+  let(:chef_run) { ChefSpec::SoloRunner.new.converge(described_recipe) }
 
   it 'converges' do
     chef_run
