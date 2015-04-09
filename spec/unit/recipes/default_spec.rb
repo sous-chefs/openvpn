3c1144d0b560e5263e1768c4a6d9e862d8415588
Chris Fordham <chris.fordham@industrieit.com>
2015-04-09 14:02:13 +1000

    Update Serverspec for default and server recipes
    - add a .rspec for color/format
    - add server spec to spec_helper.rb
    - have the default_spec only check if the openvpn package is installed
    - move default_spec to server_spec with openvpn::server recipe

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -2,22 +2,14 @@ require 'spec_helper'
 
 describe 'openvpn::default' do
   let(:chef_run) do
-    ChefSpec::SoloRunner.new(step_into: ['openvpn_conf']) do |node|
-      node.set['openvpn']['push_options'] = {
-        'dhcp-options' => ['DOMAIN local',
-                           'DOMAIN-SEARCH local']
-      }
-    end.converge(described_recipe)
+    ChefSpec::SoloRunner.new.converge(described_recipe)
   end
 
   it 'converges' do
     chef_run
   end
+end
 
-  it 'makes a template with dhcp options' do
-    expect(chef_run).to render_file('/etc/openvpn/server.conf')
-      .with_content('push "dhcp-options DOMAIN local"')
-    expect(chef_run).to render_file('/etc/openvpn/server.conf')
-      .with_content('push "dhcp-options DOMAIN-SEARCH local"')
-  end
+describe package('openvpn') do
+  it { should be_installed }
 end
