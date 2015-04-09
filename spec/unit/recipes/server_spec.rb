cb8830339f4aba0835addd468636bf36bf6b9054
Chris Fordham <chris.fordham@industrieit.com>
2015-04-09 19:50:17 +1000

    Add multiple push_routes as a test to rspec.

--- spec/unit/recipes/server_spec.rb
+++ spec/unit/recipes/server_spec.rb
@@ -7,6 +7,9 @@ describe 'openvpn::server' do
         'dhcp-options' => ['DOMAIN local',
                            'DOMAIN-SEARCH local']
       }
+      node.set['openvpn']['push_routes'] = [
+        '192.168.10.0 255.255.255.0', '10.12.10.0 255.255.255.0'
+      ]
     end.converge(described_recipe)
   end
 
@@ -14,10 +17,17 @@ describe 'openvpn::server' do
     chef_run
   end
 
-  it 'makes a template with dhcp options' do
+  it 'makes a server.conf from template with dhcp options' do
     expect(chef_run).to render_file('/etc/openvpn/server.conf')
       .with_content('push "dhcp-options DOMAIN local"')
     expect(chef_run).to render_file('/etc/openvpn/server.conf')
       .with_content('push "dhcp-options DOMAIN-SEARCH local"')
+  end
+
+  it 'makes a server.conf from template with multiple push routes' do
+    expect(chef_run).to render_file('/etc/openvpn/server.conf')
+      .with_content('push "route 192.168.10.0 255.255.255.0"')
+    expect(chef_run).to render_file('/etc/openvpn/server.conf')
+      .with_content('push "route 10.12.10.0 255.255.255.0"')
   end
 end
