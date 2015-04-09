6de23104fcf1e0dd5ab4300d457f75b05f9ed711
Chris Fordham <chris.fordham@industrieit.com>
2015-04-09 15:44:53 +1000

    Re-jig files addressing Organize the tests (pull #51).

--- test/integration/server/serverspec/default_spec.rb
+++ test/integration/server/serverspec/default_spec.rb
@@ -1,12 +1,6 @@
 # Encoding: utf-8
 require 'spec_helper'
 
-context 'Config' do
-  describe file('/etc/openvpn/server.conf') do
-    describe '#content' do
-      subject { super().content }
-      it { is_expected.to include 'push "dhcp-option DOMAIN local"' }
-      it { is_expected.to include 'push "dhcp-option DOMAIN-SEARCH local"' }
-    end
-  end
+describe package('openvpn') do
+  it { is_expected.to be_installed }
 end
