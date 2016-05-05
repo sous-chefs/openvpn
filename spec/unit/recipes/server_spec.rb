ada1a3848322e610659c7035b8163b8f2a5b6ae6
Eric Herot <eric.github@herot.com>
2016-05-05 16:07:15 -0400

    Test that CRL was generated correctly

--- spec/unit/recipes/server_spec.rb
+++ spec/unit/recipes/server_spec.rb
@@ -10,6 +10,7 @@ describe 'openvpn::server' do
       node.set['openvpn']['push_routes'] = [
         '192.168.10.0 255.255.255.0', '10.12.10.0 255.255.255.0'
       ]
+      # node.set['openvpn']['key']['org'] = 'testorg'
     end.converge(described_recipe)
   end
 
@@ -29,5 +30,23 @@ describe 'openvpn::server' do
       .with_content('push "route 192.168.10.0 255.255.255.0"')
     expect(chef_run).to render_file('/etc/openvpn/server.conf')
       .with_content('push "route 10.12.10.0 255.255.255.0"')
+  end
+
+  it 'executes gencrl with correction parameters' do
+    expect(chef_run).to run_execute('gencrl').with(
+      environment: { 'KEY_CN' => 'Fort Funston CA' },
+      command: 'openssl ca -config /etc/openvpn/easy-rsa/openssl.cnf -gencrl ' \
+               '-keyfile /etc/openvpn/keys/server.key ' \
+               '-cert /etc/openvpn/keys/server.crt ' \
+               '-out /etc/openvpn/keys/crl.pem',
+      creates: '/etc/openvpn/keys/crl.pem'
+    )
+  end
+
+  it 'creates a world readable CRL file' do
+    expect(chef_run).to create_remote_file('/etc/openvpn/crl.pem').with(
+      mode: 0644,
+      source: 'file:///etc/openvpn/keys/crl.pem'
+    )
   end
 end
--- test/integration/server/serverspec/server_spec.rb
+++ test/integration/server/serverspec/server_spec.rb
@@ -16,4 +16,20 @@ context 'Config' do
       it { is_expected.to include '-md sha256' }
     end
   end
+
+  describe file('/etc/openvpn/keys/crl.pem') do
+    describe '#content' do
+      subject { super().content }
+    end
+    it { is_expected.to be_file }
+  end
+
+  describe command('openssl crl -in /etc/openvpn/keys/crl.pem -noout -issuer') do
+    its(:stdout) do
+      is_expected.to eq(
+        'issuer=/C=US/ST=CA/L=San Francisco/O=Fort Funston/OU=OpenVPN ' \
+        "Server/CN=server/emailAddress=admin@foobar.com\n"
+      )
+    end
+  end
 end
