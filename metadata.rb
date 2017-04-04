0e4989e3484b24b2812c8546e8358d1021f51a3c
Tim Smith <tsmith@chef.io>
2017-04-03 18:58:56 -0700

    Add chef_version to the metadata
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -7,6 +7,7 @@ description       'Installs and configures openvpn and includes rake tasks for m
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url        'https://github.com/xhost-cookbooks/openvpn'
 issues_url        'https://github.com/xhost-cookbooks/openvpn/issues'
+chef_version      '>= 12.1'
 
 recipe 'openvpn::default',              'Installs OpenVPN only (no configuration).'
 recipe 'openvpn::install',              'Installs OpenVPN only (no configuration).'
