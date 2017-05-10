12b769d28557d6e890e54ff9227efc954563e2d1
Ed Ropple <ed@edropple.com>
2017-05-10 13:51:24 -0400

    resource WIP

--- resources/user.rb
+++ resources/user.rb
@@ -3,17 +3,23 @@
 # Resource:: user
 #
 
-property :name, String, name_property: true
+property :client_name, String, name_property: true
 property :force, [true, false]
+property :destination, String
 
-key_dir = node['openvpn']['key_dir']
-cert_path = [key_dir, "#{name}.crt"].join("/")
-client_file_basename = [node['openvpn']['client_prefix'], name].join('-')
-bundle_filename = "#{name}.tar.gz"
-
+# TODO: this action will not recreate if the client configuration data has
+#       changed. Requires manual intervention.
 action :create do
-  execute "generate-openvpn-#{name}" do
-    command "./pkitool #{name}"
+  key_dir = node['openvpn']['key_dir']
+  cert_path = ::File.join(key_dir, "#{client_name}.crt")
+  client_file_basename = [node['openvpn']['client_prefix'], name].join('-')
+  bundle_filename = "#{client_name}.tar.gz"
+
+  bundle_full_path = ::File.expand_path("#{key_dir}/bundle_filename")
+  destination_file = File.expand_path(destination || bundle_full_path)
+
+  execute "generate-openvpn-#{client_name}" do
+    command "./pkitool #{client_name}"
     cwd '/etc/openvpn/easy-rsa'
     environment(
       'EASY_RSA'     => '/etc/openvpn/easy-rsa',
@@ -31,25 +37,48 @@ action :create do
     not_if { ::File.exist?(cert_path) }
   end
 
+  cleanup_name = "cleanup-old-bundle-#{client_name}"
+
   %w(conf ovpn).each do |ext|
-    template "#{key_dir}/#{client_file_basename}.#{ext}" do
+    filename = "#{key_dir}/#{client_file_basename}.#{ext}"
+    template filename do
       source 'client.conf.erb'
       cookbook node['openvpn']['cookbook_user_conf']
       variables(client_cn: name)
+
+      notifies :delete, "file[#{cleanup_name}]", :immediately
     end
   end
 
-  execute "create-openvpn-tar-#{name}" do
+  file cleanup_name do
+    action :nothing
+
+    path destination_file
+  end
+
+  execute "create-openvpn-tar-#{client_name}" do
     cwd key_dir
     command <<-EOH
       tar zcf #{bundle_filename} \
-        ca.crt #{name}.crt \
-        #{name}.key \
+        ca.crt #{client_name}.crt \
+        #{client_name}.key \
         #{client_file_basename}.conf \
         #{client_file_basename}.ovpn
     EOH
-    only_if do
-      !force && !::File.exist?("#{key_dir}/#{bundle_filename}")
+
+    not_if { !force && ::File.exist?(destination_file) }
+  end
+
+
+
+  execute "move-bundle-to-destination-#{client_name}" do
+    command <<-EOH
+      mv #{bundle_filename_real} #{destination_file}
+    EOH
+
+    not_if do
+      bundle_full_path != destination_file ||
+        (!force && ::File.exist?(destination_file))
     end
   end
 end
