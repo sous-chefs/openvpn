17320cb08a4566c64bf39be22f33c02bcbc96fd5
Tim Smith <tsmith@chef.io>
2017-08-07 15:14:59 -0700

    Use SPDX compliant license string
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
 name              'openvpn'
 version           '3.0.0'
-license           'Apache 2.0'
 maintainer        'Sous Chefs'
 maintainer_email  'help@sous-chefs.org'
+license           'Apache-2.0'
 description       'Installs and configures openvpn and includes rake tasks for managing certs.'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url        'https://github.com/xhost-cookbooks/openvpn'
