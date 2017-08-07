174d1d4c239a90f2645aaa2b255088e273c55024
Tim Smith <tsmith@chef.io>
2017-08-07 15:20:43 -0700

    Convert the LWRP to a custom resource
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- resources/conf.rb
+++ resources/conf.rb
@@ -16,13 +16,52 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-actions :create, :delete
-default_action :create
+property :cookbook, kind_of: String, default: 'openvpn'
+property :config, Hash
+property :push_routes, Array
+property :push_options, Array
 
-attribute :cookbook, kind_of: String, default: 'openvpn'
-attribute :config,
-          kind_of: Hash
-attribute :push_routes,
-          kind_of: Array
-attribute :push_options,
-          kind_of: Array
+action :create do
+  # FreeBSD service uses openvpn.conf
+  template_source = if new_resource.name == 'openvpn'
+                      'server.conf.erb'
+                    else
+                      "#{new_resource.name}.conf.erb"
+                    end
+
+  template [node['openvpn']['fs_prefix'], "/etc/openvpn/#{new_resource.name}.conf"].join do
+    cookbook new_resource.cookbook
+    source template_source
+    owner 'root'
+    group node['openvpn']['root_group']
+    mode '644'
+    variables(
+      config: new_resource.config || node['openvpn']['config'],
+      push_routes: new_resource.push_routes || node['openvpn']['push_routes'],
+      push_options: new_resource.push_options || node['openvpn']['push_options'],
+      client_cn: node['openvpn']['client_cn']
+    )
+    helpers do
+      def render_push_options(push_options)
+        return [] if push_options.nil?
+        push_options.each_with_object([]) do |(option, conf), m|
+          case conf
+          when Chef::Node::ImmutableArray, Array
+            conf.each { |o| m << "push \"#{option} #{o}\"" }
+          when String
+            m << "push \"#{option} #{conf}\""
+          else
+            raise "Push option data type #{conf.class} not supported"
+          end
+        end
+      end
+      # rubocop:enable Metrics/MethodLength
+    end
+  end
+end
+
+action :delete do
+  file [node['openvpn']['fs_prefix'], "/etc/openvpn/#{new_resource.name}.conf"].join do
+    action :delete
+  end
+end
