8a52c938527acdde72e3b72355a70e4cb26fe706
Axel Amigo Arnold <axl89@users.noreply.github.com>
2020-10-08 08:30:27 +0200

    Install gpg package (#183)
    This package is required for apt-repo recipe to converge successfully.

--- recipes/apt-repo.rb
+++ recipes/apt-repo.rb
@@ -3,6 +3,15 @@
 # Recipe:: apt-repo
 #
 
+apt_update 'all platforms' do
+  action :nothing
+end
+
+apt_package 'gpg' do
+  action :install
+  notifies :update, 'apt_update[all platforms]', :before
+end
+
 apt_repository 'openvpn' do
   uri        'http://build.openvpn.net/debian/openvpn/stable/'
   key        'https://swupdate.openvpn.net/repos/repo-public.gpg'
