2d41e862dd7b8b402bb39958aa82a501fda490f0
David Headrick <daviddjh@osuosl.org>
2021-12-15 14:12:03 -0800

    Added tests for user resource
    Signed-off-by: David Headrick <daviddjh@osuosl.org>

--- test/integration/server_verification/default_test.rb
+++ test/integration/server_verification/default_test.rb
@@ -10,3 +10,16 @@
     end
   end
 end
+
+describe file('/etc/openvpn/keys/vpn_user.crt') do
+    describe '#content' do
+      subject { super().content }
+      it { is_expected.to include 'C=CA' }
+      it { is_expected.to include 'ST=Ontario' }
+      it { is_expected.to include 'L=Ottawa' }
+      it { is_expected.to include 'O=Test Org' }
+      it { is_expected.to include 'OU=Test Org Unit' }
+      it { is_expected.to include 'CN=vpn_user/emailAddress=vpn_user@test.com' }
+      it { is_expected.to include 'Public-Key: (1024 bit)' }
+    end
+end
